35c16631527b15b9dced274052b2ffb5bb92e8ca
João Dal Lago <joaogabrieldallago@hotmail.com>
2024-06-30 21:19:12 -0300

    :sparkles: Feat: Model Contact

---
+++ app/Models/Contact.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $table = 'contact';
+
+    protected $fillable = [
+        'user_id',
+        'phone',
+        'professional_email',
+        'linkedin_url',
+        'instagram_url',
+        'whatsapp_url'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -20,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'cau'
     ];
 
     /**
@@ -43,5 +45,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * @return HasOne
+     */
+    public function contact(): HasOne
+    {
+        return $this->hasOne(Contact::class);
     }
 }
