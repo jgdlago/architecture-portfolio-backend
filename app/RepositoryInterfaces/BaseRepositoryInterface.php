56611ad90ef6e64e2974b79641cfcccfcba2a430
João Dal Lago <joaogabrieldallago@hotmail.com>
2024-06-30 22:14:00 -0300

    :sparkles: Feat: BaseRepository

---
+++ app/RepositoryInterfaces/BaseRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\RepositoryInterfaces;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+interface BaseRepositoryInterface
+{
+    public function getAllModel(): LengthAwarePaginator;
+
+    public function getModelById(int|Model $modelId): Model;
+
+    public function createModel(array $modelDetails): Model;
+
+    public function updateModel(array $modelDetails, Model|int $modelId): Model;
+
+    public function deleteModel(Model|int $modelOrId): ?bool;
+}
