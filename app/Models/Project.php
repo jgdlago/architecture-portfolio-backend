39df4ce3f94cf2d302898a324e62eaadb1a315db
João Dal Lago <joaogabrieldallago@hotmail.com>
2024-06-30 22:06:39 -0300

    :sparkles: Feat: model project e image

---
+++ app/Models/Project.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $table = 'projects';
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'city',
+        'description',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProjectImage::class);
+    }
+}
